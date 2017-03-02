0379ef0e3986f36d928fb5aeb1c2de37c0df0901
ivillaabrille <isayavillaabrille@domain.com>
2017-03-02 20:49:37 +0800

    first commit

---
+++ public/js/json.js
@@ -0,0 +1,21 @@
+//console.log("Hello World");
+function x(){
+	var fn = document.getElementById('fn').value;
+	var sn = document.getElementById('sn').value;
+	var sum = parseInt(fn) + parseInt(sn);
+	// console.log("The sum is "+ sum);
+	document.querySelector('.result').innerHTML = sum;
+}
+
+$(function(){
+	$.ajax({
+		url:"http://www.omdbapi.com/?t=Game%20of%20Thrones"
+	}).done(function(data){
+		console.log(data);
+		let html = `
+			<h1>${data.Title}</h1>
+			<img src="${data.Poster}" />
+		`;
+		document.querySelector('.app').innerHTML = html;
+	});
+})
